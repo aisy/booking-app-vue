56cdad7da7e00e782a9433a9fc51397d22c48cc5
aisy <aisymuhammad@gmail.com>
2020-07-31 20:28:45 +0700

    add content from sample with loading

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,18 +4,46 @@
       <v-container>
         <v-row>
           <v-col cols="3">
-            <v-card>
+            <v-card style="margin-top:12px">
               <v-card-text>
-                <v-text-field label="Nama Ruangan" solo v-model="nama_ruangan" rounded />
+                <v-text-field label="Cari Nama Ruangan..." solo v-model="nama_ruangan" rounded />
                 <div v-for="list in genre" :key="list">
-                  <v-checkbox v-model="dataFilter" class="mx-2" :label="list" :value="list"></v-checkbox>
+                  <v-checkbox v-model="showDialog" class="mx-2" :label="list" :value="list"></v-checkbox>
                 </div>
-                <v-btn color="primary" @click="onClick()">Cari</v-btn>
+                <v-btn color="primary" @click="SearchOnClick()">Cari</v-btn>
               </v-card-text>
             </v-card>
           </v-col>
 
-          <v-col cols="9">
+          <!-- modal -->
+          <v-dialog v-model="showDialog" max-width="600">
+            <v-card>
+              <v-card-title class="headline">Apakah anda akan membooking {{dataDialog.title}}?</v-card-title>
+              <v-card-text>Let Google help apps determine location. This means sending anonymous location data to Google, even when no apps are running.</v-card-text>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn color="green darken-1" text @click="showDialog = false">Batalkan</v-btn>
+                <v-btn color="green darken-1" dark @click="showDialog = false">Booking</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-dialog>
+
+          <!-- Loading -->
+          <v-col cols="9" v-if="loading">
+            <v-row>
+              <v-col cols="4" v-for="n in 4" :key="n">
+                <v-card>
+                  <v-skeleton-loader type="image" :loading="true" height="200" />
+                  <v-card-text>
+                    <v-skeleton-loader :loading="true" height="94" type="list-item-two-line" />
+                    <v-skeleton-loader type="button" />
+                  </v-card-text>
+                </v-card>
+              </v-col>
+            </v-row>
+          </v-col>
+
+          <v-col cols="9" v-if="!loading">
             <v-row>
               <v-col cols="4" v-for="list in dataFilter" :key="list.title">
                 <v-card>
@@ -29,6 +57,7 @@
                   <v-card-text>
                     <p>{{list.title}}</p>
                     <p>{{list.releaseYear}}</p>
+                    <v-btn color="success" @click="bookingProgress(list)">Booking</v-btn>
                   </v-card-text>
                 </v-card>
               </v-col>
@@ -48,44 +77,52 @@ export default {
   components: {},
   data() {
     return {
+      loading: true,
       tanggal: "",
       waktu: "",
       nama_ruangan: "",
       genre: ["action", "sci-fi"],
       listGenre: [],
       dataFilter: [],
-      dataPrevious: []
+      dataPrevious: [],
+      showDialog: false,
+      dataDialog: {},
     };
   },
   methods: {
-    onClick() {
+    searchOnClick() {
       if (this.nama_ruangan === "") {
         this.dataFilter = this.dataPrevious;
       } else {
-        const filtering = this.dataFilter.filter(items => {
+        const filtering = this.dataFilter.filter((items) => {
           return items.title
             .toLowerCase()
             .includes(this.nama_ruangan.toLowerCase());
         });
         this.dataFilter = filtering;
       }
-    }
+    },
+    bookingProgress(dataDialog) {
+      this.showDialog = true;
+      this.dataDialog = dataDialog;
+    },
   },
   computed: {
     allData() {
       return this.dataMovies;
-    }
+    },
   },
   mounted() {
     axios
       .get("https://reactnative.dev/movies.json")
-      .then(items => {
+      .then((items) => {
         this.dataFilter = items.data.movies;
         this.dataPrevious = items.data.movies;
+        this.loading = false;
       })
-      .catch(err => {
+      .catch((err) => {
         console.warn(err);
       });
-  }
+  },
 };
 </script>
