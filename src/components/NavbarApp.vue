29a68e85e8c3401eb84aadf70b9deb5f6c3112ac
aisy <aisymuhammad@gmail.com>
2020-07-31 21:46:52 +0700

    change color, remove unused code and add icon button

--- src/components/NavbarApp.vue
+++ src/components/NavbarApp.vue
@@ -1,30 +1,13 @@
 <template>
-  <v-app-bar app color="primary" dark>
+  <v-app-bar app color="deep-purple accent-4" dark>
     <div class="d-flex align-center">
-      <v-img
-        alt="Vuetify Logo"
-        class="shrink mr-2"
-        contain
-        src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-        transition="scale-transition"
-        width="40"
-      />
-
-      <v-img
-        alt="Vuetify Name"
-        class="shrink mt-1 hidden-sm-and-down"
-        contain
-        min-width="100"
-        src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-        width="100"
-      />
+      <v-toolbar-title>Bookingin</v-toolbar-title>
     </div>
 
     <v-spacer></v-spacer>
 
-    <v-btn href="https://github.com/vuetifyjs/vuetify/releases/latest" target="_blank" text>
-      <span class="mr-2">Latest Release</span>
-      <v-icon>mdi-open-in-new</v-icon>
+    <v-btn rounded color="primary">
+      <v-icon>account_circle</v-icon>User
     </v-btn>
   </v-app-bar>
 </template>
