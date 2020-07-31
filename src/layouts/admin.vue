12f6098295ad4be1f8f17349299f5f3bd29b7764
aisy <aisymuhammad@gmail.com>
2020-07-31 21:47:43 +0700

    add Layout to handle parent page in Vue

---
+++ src/layouts/admin.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-main>
+    <Navbar />
+    <router-view />
+  </v-main>
+</template>
+
+<script>
+import Navbar from "../components/NavbarApp";
+
+export default {
+  components: {
+    Navbar,
+  },
+};
+</script>
